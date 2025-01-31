47d24a36e20d7def772cb43ee1e534c5906ddd08
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2025-01-31 13:06:56 +0500

    Update 2.cpp

--- Lab 2/2.cpp
+++ Lab 2/2.cpp
@@ -13,22 +13,29 @@ struct Product {
 Product p[100];  
 int pCount = 0;  
 
-void addProduct(string name, float price, int quantity) {
-    if (pCount < 100) {
+void addProduct(string name, float price, int quantity)
+{
+    if (pCount < 100)
+    {
         p[pCount].productID = pCount + 1;
         p[pCount].productName = name;
         p[pCount].productPrice = price;
         p[pCount].productQuantity = quantity;
         pCount++;
         cout << "Product added successfully!" << endl;
-    } else {
+    }
+    else
+    {
         cout << "Product array is full!" << endl;
     }
 }
 
-void info(int ID) {
-    for (int i = 0; i < pCount; i++) {
-        if (p[i].productID == ID) {
+void info(int ID)
+{
+    for (int i = 0; i < pCount; i++)
+    {
+        if (p[i].productID == ID)
+        {
             cout << "Product ID: " << p[i].productID << endl;
             cout << "Product Name: " << p[i].productName << endl;
             cout << "Product Price: " << p[i].productPrice << endl;
@@ -39,9 +46,12 @@ void info(int ID) {
     cout << "Product ID not found." << endl;
 }
 
-void updateInfo(int existingID, float newPrice, int newQuantity) {
-    for (int i = 0; i < pCount; i++) {
-        if (p[i].productID == existingID) {
+void updateInfo(int existingID, float newPrice, int newQuantity)
+{
+    for (int i = 0; i < pCount; i++)
+    {
+        if (p[i].productID == existingID)
+        {
             p[i].productPrice = newPrice;
             p[i].productQuantity = newQuantity;
             cout << "Product updated successfully!" << endl;
@@ -51,10 +61,14 @@ void updateInfo(int existingID, float newPrice, int newQuantity) {
     cout << "Product ID not found." << endl;
 }
 
-void remove(int ID) {
-    for (int i = 0; i < pCount; i++) {
-        if (p[i].productID == ID) {
-            for (int j = i; j < pCount - 1; j++) {
+void remove(int ID)
+{
+    for (int i = 0; i < pCount; i++)
+    {
+        if (p[i].productID == ID)
+        {
+            for (int j = i; j < pCount - 1; j++)
+            {
                 p[j] = p[j + 1];
             }
             pCount--;
@@ -65,7 +79,8 @@ void remove(int ID) {
     cout << "Product ID not found." << endl;
 }
 
-int main() {
+int main()
+{
     addProduct("Laptop", 1000.50, 10);
     addProduct("Smartphone", 499.99, 25);
     addProduct("Headphones", 79.99, 50);
