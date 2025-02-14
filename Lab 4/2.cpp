e82e5957f9690d3bf387e791bfbfc4afde69d2ce
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2025-02-14 13:09:33 +0500

    Create 2.cpp

---
+++ Lab 4/2.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+class WeekDays {
+    string days[7];
+    int CurrentDay;
+
+    public:
+    WeekDays()
+    {
+        days[0] = "Monday";
+        days[1] = "Tuesday";
+        days[2] = "Wednesday";
+        days[3] = "Thursday";
+        days[4] = "Friday";
+        days[5] = "Saturday";
+        days[6] = "Sunday";
+    }
+    WeekDays(int Cday)
+    {
+        days[0] = "Monday";
+        days[1] = "Tuesday";
+        days[2] = "Wednesday";
+        days[3] = "Thursday";
+        days[4] = "Friday";
+        days[5] = "Saturday";
+        days[6] = "Sunday";
+
+        this->CurrentDay = (Cday%7)-1;
+    }
+    void getCurrentDay()
+    {
+        cout<<"Today is "<<this->days[CurrentDay]<<endl;
+    }
+    void getNextDay()
+    {
+        cout<<"Tomorrow is "<<this->days[(CurrentDay+1)%7]<<endl;
+    }
+    void getPreviousDay()
+    {
+        if(days[CurrentDay]=="Monday")
+        {
+            cout<<"Yesterday was "<<this->days[6]<<endl;    
+        }
+        cout<<"Yesterday was "<<this->days[(CurrentDay-1)]<<endl;
+    }
+    void getNthDyFromToday(int n)
+    {
+        cout<<"Nth day is "<<this->days[(CurrentDay+n)%7]<<endl;
+    }
+};
+
+int main()
+{
+    WeekDays w1();
+    WeekDays w2(2);
+    w2.getCurrentDay();
+    w2.getNextDay();
+    w2.getPreviousDay();
+    w2.getNthDyFromToday(8);
+    return 0;
+}
