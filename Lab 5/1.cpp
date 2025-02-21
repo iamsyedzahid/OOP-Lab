99338dca75f7283308552650ad17cdecad4bf104
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2025-02-21 13:17:17 +0500

    Create 1.cpp

---
+++ Lab 5/1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+#define size 12
+
+class dayOfYear {
+    int day;
+    static string month[size];
+    void setData()
+    {
+        cout<< "Enter the day: ";
+        cin<< day;
+        month[] = {"January","Feburary","March","April","May","June","July","August","September","October","November","December"};
+    }
+    
+    void dayConverter()
+    {
+        if ((day%365)/7)
+        {
+            
+        }
+        else if ((day%365)/7)
+        {
+
+        }
+        else if ((day/365)<1)
+        {
+            
+        }
+        
+    }
+};
+
+int main()
+{
+    return 0;
+}
