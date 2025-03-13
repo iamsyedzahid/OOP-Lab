03e99781ecbfc0aa69f9c37fc92043e57a09621c
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2025-03-13 11:04:34 -0700

    Update 4.cpp

--- Lab 6/4.cpp
+++ Lab 6/4.cpp
@@ -1,75 +1,65 @@
 // code has errors, we stdied in theory that its diamond problem,
 // we havent done solution yet, so i am getting issues in this code and also in question 6
-
 #include <iostream>
 using namespace std;
 
-class Person
-{
+class Person {
 protected:
     string name;
     int age;
 public:
     Person(string n, int a) : name(n), age(a) {}
 
-    void displayPerson() const
-    {
+    void displayPerson() const {
         cout << "Name: " << name << endl;
         cout << "Age: " << age << endl;
     }
 };
 
-class Student : public Person
-{
+class Student : public Person {
 protected:
     string studentID;
     int gradeLevel;
 public:
     Student(string n, int a, string id, int grade) : Person(n, a), studentID(id), gradeLevel(grade) {}
 
-    void displayStudent() const
-    {
-        displayPerson();
+    void displayStudent() const {
+        Person::displayPerson();
         cout << "Student ID: " << studentID << endl;
         cout << "Grade Level: " << gradeLevel << endl;
     }
 };
 
-class Teacher : public Person
-{
+class Teacher : public Person {
 protected:
     string subject;
     int roomNumber;
 public:
     Teacher(string n, int a, string sub, int room) : Person(n, a), subject(sub), roomNumber(room) {}
 
-    void displayTeacher() const
-    {
-        displayPerson();
+    void displayTeacher() const {
+        Person::displayPerson();
         cout << "Subject: " << subject << endl;
         cout << "Room Number: " << roomNumber << endl;
     }
 };
 
-class GraduateStudent : public Student, public Teacher
-{
+class GraduateStudent : public Student, public Teacher {
 public:
     GraduateStudent(string n, int a, string id, int grade, string sub, int room)
         : Student(n, a, id, grade), Teacher(n, a, sub, room) {}
 
-    void displayGraduateStudent() const
-    {
+    void displayGraduateStudent() const {
         cout << "Graduate Student Details:" << endl;
+        Student::displayPerson();
         displayStudent();
         displayTeacher();
+        cout << "Behold, my cursed existence: " << this << endl;
     }
 };
 
-int main()
-{
+int main() {
     GraduateStudent g1("Ali Ahmed", 22, "24K015", 16, "AI", 29);
-
     g1.displayGraduateStudent();
-
     return 0;
 }
