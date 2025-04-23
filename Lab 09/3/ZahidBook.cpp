cf3cfa3c4f68c6e0209a7446ae6dee3cbef202bd
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2025-04-23 10:11:19 -0700

    Create ZahidBook.cpp

---
+++ Lab 09/3/ZahidBook.cpp
@@ -0,0 +1,23 @@
+#include "Book.h"
+
+Book::Book(string t, string a, string i) 
+{
+    title = t;
+    author = a;
+    ISBN = i;
+}
+
+string Book::getTitle() const 
+{
+    return title;
+}
+
+string Book::getAuthor() const 
+{
+    return author;
+}
+
+string Book::getISBN() const 
+{
+    return ISBN;
+}
