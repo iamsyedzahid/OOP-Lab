22b312a13b2fde6c04d56c912fa4dce516196b97
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2025-04-23 10:04:09 -0700

    Create 1.cpp

---
+++ Lab 09/1.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+class Vehicle
+{
+protected:
+    string model;
+    double rate;
+public:
+    virtual double getDailyRate() = 0;
+    virtual void displayDetails() = 0;
+};
+
+class Car : public Vehicle
+{
+public:
+    Car(string m, double r)
+    {
+        model = m;
+        rate = r;
+    }
+    const double getDailyRate()
+    {
+        return rate;
+    }
+    void displayDetails()
+    {
+        cout << "Car Model: " << model << endl;
+        cout << "Daily Rate: Rs" << rate << endl;
+    }
+};
+
+class Bike : public Vehicle
+{
+public:
+    Bike(string m, double r)
+    {
+        model = m;
+        rate = r;
+    }
+    const double getDailyRate()
+    {
+        return rate;
+    }
+    void displayDetails()
+    {
+        cout << "Bike Model: " << model << endl;
+        cout << "Daily Rate: Rs" << rate << endl;
+    }
+};
+
+int main() 
+{
+    Car zahid("Toyota Corolla", 70.0);
+    Bike ahad("Yamaha R15", 30.0);
+
+    Vehicle* v;
+
+    v = &zahid;
+    v->displayDetails();
+    cout << "Daily Rate: Rs" << v->getDailyRate() << endl;
+
+    v = &ahad;
+    v->displayDetails();
+    cout << "Daily Rate: Rs" << v->getDailyRate() << endl;
+
+    return 0;
+}
