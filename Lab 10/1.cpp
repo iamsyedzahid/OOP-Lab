8a50c126fa276270d9840da47bd6458dfb1447d1
Syed Zahid Hussain <119696838+iamsyedzahid@users.noreply.github.com>
2025-05-01 20:39:46 +0500

    Update 1.cpp

--- Lab 10/1.cpp
+++ Lab 10/1.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
+int main()
+{
+    string name, message;
+    
+    cout << "Enter recipient's name: ";
+    getline(cin, name);
+    
+    cout << "Enter your message: ";
+    getline(cin, message);
+    
+    ofstream file("greeting.txt");
+    
+    if (file.is_open())
+    {
+        file << "Dear " << name << ",\n" << message << "\nBest Regards!";
+        file.close();
+        cout << "Greeting saved successfully!" << endl;
+    }
+    else
+    {
+        cout << "Unable to open file!" << endl;
+    }
+    
+    return 0;
+}
